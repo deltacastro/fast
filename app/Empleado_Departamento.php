41384e1a3a201a0f736329b3401151393e8598ae
deltacastro <abel.castro9111@gmail.com>
2020-03-12 20:39:28 +0000

    Se agrega trait softdeletes

--- app/Empleado_Departamento.php
+++ app/Empleado_Departamento.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Empleado_Departamento extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'empleados_has_departamentos';
     protected $fillable = [
         'empleado_id', 'cargo_id', 'departamento_id'
