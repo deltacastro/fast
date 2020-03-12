b3e7f53e3d791dc97c57b92af6ebdd766efefd2f
deltacastro <abel.castro9111@gmail.com>
2020-03-12 19:59:14 +0000

    Se agrega migracion y modelo para la tabla People

---
+++ app/People.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class People extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'nombre', 'paterno', 'materno'
+    ];
+}
