33e3e3f9e58f27614a4f0d2fbc98177e53ff8df3
deltacastro <abel.castro9111@gmail.com>
2020-03-13 21:22:42 +0000

    Se crea script para agregar departamento y cargo en una tabla de manera dinamica.

--- resources/views/admin/user/_form.blade.php
+++ resources/views/admin/user/_form.blade.php
@@ -67,38 +67,42 @@
     <h4>Puestos</h4>
     <div class="row">
         <div class="form-group col-12 col-lg-5">
-            <label for="empleado_departamento[departamento_id]">Departamento</label>
+            <label for="empleado_departamento">Departamento</label>
             {{-- <input class="form-control" type="text" name="empleado_departamento[departamento_id]" value="{{ isset($user) ? '' : '' }}"> --}}
-            <select class="custom-select" name="empleado_departamento[departamento_id]" id="">
-                <option value="null" selected disabled>Seeccione un departamento</option>
-                <option value="">Opc 1</option>
-                <option value="">Opc 2</option>
-                <option value="">Opc 3</option>
+            <select class="custom-select" id="empleado_departamento">
+                {{-- <option value="null" selected disabled>Seeccione un departamento</option> --}}
+                @forelse ($departamentos as $departamento)
+                    <option value="{{$departamento->id}}" title="{{$departamento->descripcion}}">{{$departamento->nombre}}</option>
+                @empty
+                    <option value="null" disabled>No hay cargos agregados...</option>
+                @endforelse
 
             </select>
             <span class="invalid-feedback" role="alert">
             </span>
         </div>
         <div class="form-group col-12 col-lg-5">
-            <label for="empleado_departamento[cargo_id]">Cargo</label>
+            <label for="empleado_cargo">Cargo</label>
             {{-- <input class="form-control" type="text" name="empleado_departamento[cargo_id]" value="{{ isset($user) ? '' : '' }}"> --}}
-            <select class="custom-select" name="empleado_departamento[cargo_id]" id="">
-                <option value="null" selected disabled>Seleccione un cargo</option>
-                <option value="">Opc 1</option>
-                <option value="">Opc 2</option>
-                <option value="">Opc 3</option>
+            <select class="custom-select" name="empleado_cargo" id="empleado_cargo">
+                {{-- <option value="null" selected disabled>Seleccione un cargo</option> --}}
+                @forelse ($cargos as $cargo)
+                    <option value="{{$cargo->id}}" title="{{$cargo->descripcion}}">{{$cargo->nombre}}</option>
+                @empty
+                    <option value="null" disabled>No hay cargos agregados...</option>
+                @endforelse
             </select>
             <span class="invalid-feedback" role="alert">
             </span>
         </div>
         <div class="form-group col-12 col-lg-2">
             <label for="">.</label>
-            <button class="form-control btn btn-primary">Agregar</button>
+            <button type="button" id="departamento-cargo-agregar" class="form-control btn btn-primary">Agregar</button>
         </div>
     </div>
     <div class="table-responsive">
 
-        <table class="table table-striped">
+        <table id="table-departamento-cargo" class="table table-striped">
             <thead class="thead-light">
                 <tr>
                     <th>Departamento</th>
@@ -112,3 +116,62 @@
         </table>
     </div>
 </form>
+
+<script>
+    $(document).ready(function(){
+        $('#departamento-cargo-agregar').on('click', function(){
+            const table_tbody = $('#table-departamento-cargo').children('tbody')
+
+            // Se instancian los selects
+            const select_departamento = $('#empleado_departamento')
+            const select_cargo = $('#empleado_cargo')
+
+            // Se guardan los ids
+            const departamento_id = select_departamento.val()
+            const cargo_id = select_cargo.val()
+
+            // Se busca el elemento option seleccionado
+            const option_departamento = select_departamento.children('option:selected')
+            const option_cargo = select_cargo.children('option:selected')
+
+            // Obtenemos el nombre del cargo y departamento
+            const departamento_nombre = option_departamento.html()
+            const cargo_nombre = option_cargo.html()
+
+            // String aleatorio para relacion de elementos
+            const random_string = Math.random().toString(36).substring(7);
+
+            // Se crean los td's del departamento y cargo
+            const td_departamento = `
+                <input name="empleado_cargo[${random_string}][departamento_id]" value="${departamento_id}" hidden>
+                ${departamento_nombre}
+            `
+            const td_cargo = `
+                <input name="empleado_cargo[${random_string}][cargo_id]" value="${cargo_id}" hidden>
+                ${cargo_nombre}
+            `
+
+            // Se crea elemento para el boton eliminar
+            const button_delete = `
+                <button type="button" data-id="${random_string}" class="form-control btn btn-primary departamento-cargo-eliminar">Eliminar</button>
+            `
+
+            // Extructura
+            const table_row = `
+                <tr id="${random_string}">
+                    <td>
+                        ${td_departamento}
+                    </td>
+                    <td>
+                        ${td_cargo}
+                    </td>
+                    <td>
+                        ${button_delete}
+                    </td>
+                </tr>
+            `
+
+            table_tbody.append(table_row)
+        })
+    })
+</script>
