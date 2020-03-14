33f39cf95c6be0ce0a38412113e2882662d541a9
deltacastro <abel.castro9111@gmail.com>
2020-03-14 19:02:36 +0000

    Se agrega seeder para la tabla estado_civil en el main

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(DepartamentosTableSeeder::class);
         $this->call(CargosTableSeeder::class);
+        $this->call(EstadoCivilTableSeeder::class);
     }
 }
