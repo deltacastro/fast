6c53a9fee8c4a998967b1b4eaa2b8f0fcc04e8ee
deltacastro <abel.castro9111@gmail.com>
2020-03-14 21:05:30 +0000

    Se agregan funciones para estructrar el request con los datos del empleado y guardar / actualizar en bd

--- app/Empleado.php
+++ app/Empleado.php
@@ -16,4 +16,27 @@ class Empleado extends Model
     protected $dates = [
         'fecha_ingreso'
     ];
+
+    private function buildDataFillable ($data) {
+        $dataFillable = array();
+        foreach ($this->fillable as $value) {
+            $dataFillable[$value] = isset($data[$value]) ? $data[$value] : null;
+            if ($dataFillable[$value] == null) {
+                unset($dataFillable[$value]);
+            }
+        }
+        return $dataFillable;
+    }
+
+    public function guardar($data) {
+        $data = $this->buildDataFillable($data);
+        $data = $this->create($data);
+        return $data;
+    }
+
+    public function actualizar($data, $model) {
+        $data = $this->buildDataFillable($data);
+        $data = $model->fill($data)->save();
+        return $data ? $this->find($model->id) : false;
+    }
 }
