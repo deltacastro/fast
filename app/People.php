66ebc9fca70217d1754ce4939b47c1ec9adc928e
deltacastro <abel.castro9111@gmail.com>
2020-03-14 21:06:02 +0000

    Se agregan funciones para estructrar el request con los datos de la persona y guardar / actualizar en bd

--- app/People.php
+++ app/People.php
@@ -12,4 +12,27 @@ class People extends Model
     protected $fillable = [
         'nombre', 'paterno', 'materno'
     ];
+
+    private function buildDataFillable ($data) {
+        $dataFillable = array();
+        foreach ($this->fillable as $value) {
+            $dataFillable[$value] = isset($data[$value]) ? $data[$value] : null;
+            if ($dataFillable[$value] == null) {
+                unset($dataFillable[$value]);
+            }
+        }
+        return $dataFillable;
+    }
+
+    public function guardar($data) {
+        $data = $this->buildDataFillable($data);
+        $data = $this->create($data);
+        return $data;
+    }
+
+    public function actualizar($data, $model) {
+        $data = $this->buildDataFillable($data);
+        $data = $model->fill($data)->save();
+        return $data ? $this->find($model->id) : false;
+    }
 }
