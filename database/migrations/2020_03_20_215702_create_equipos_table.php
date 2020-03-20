945ca69a9540a1c5bc799ec9aa3279d573ad8c61
deltacastro <abel.castro9111@gmail.com>
2020-03-20 22:05:48 +0000

    se crea migracion para la tabla equipos

---
+++ database/migrations/2020_03_20_215702_create_equipos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEquiposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('tipo_id');
+            $table->unsignedBigInteger('responsable_id');
+            $table->string('nombre', 254);
+            $table->text('observaciones')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipos');
+    }
+}
