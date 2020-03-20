4e9a7b721aa4e365f9098a6420238147cfa944f2
deltacastro <abel.castro9111@gmail.com>
2020-03-20 00:45:30 +0000

    Se crean selects dinamicos para seleccionar el usuario y el departamento del usuario que solicita

--- resources/views/ti/servicio/_solicitante.blade.php
+++ resources/views/ti/servicio/_solicitante.blade.php
@@ -1,10 +1,30 @@
-<h3>Datos del colaborador</h3>
+<h3>Datos del solicitante</h3>
 <div class="row">
-    <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+    <div class="col-12 col-md-12 col-lg-6">
         <div class="form-group row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Nombre</label>
+            <label for="select_userId" class="col-sm-2 col-form-label">Nombre</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="">
+                <select id="select_userId" class="form-control" data-target="#select-departamentoId" name="user[id]">
+                    @forelse ($users as $user)
+                        <option value="{{$user->id}}" title="{{$user->persona->nombreCompleto()}}">{{$user->persona->nombre}}</option>
+                    @empty
+                        <option value="null" disabled>...</option>
+                    @endforelse
+                </select>
+                <span class="invalid-feedback" role="alert">
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-md-12 col-lg-6">
+        <div class="form-group row">
+            <label for="select-departamentoId" class="col-sm-2 col-form-label">Departamento</label>
+            <div class="col-sm-10">
+                <select id="select-departamentoId" class="custom-select" name="departamento[id]">
+
+                </select>
+                <span class="invalid-feedback" role="alert">
+                </span>
             </div>
         </div>
     </div>
