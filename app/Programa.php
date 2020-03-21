a7e93d7baa78828d930bc00524fca6029d0a8932
deltacastro <abel.castro9111@gmail.com>
2020-03-21 18:40:42 +0000

    Se crea clase para la tabla programas

---
+++ app/Programa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Programa extends Model
+{
+    use SoftDeletes;
+    protected $table = 'programas';
+    protected $fillable = [
+        'nombre'
+    ];
+}
